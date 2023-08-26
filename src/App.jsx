bab54966a19cd7af0313e7ff4ad9d15ea62169c4
Risma <muhammad.risma@student.ac.id>
2023-08-26 19:55:11 +0700

    Adding form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
-
+import Meme from './components/Meme'
 
 function App() {
   return (
     <>
       <Header/>
+      <Meme/>
     </>
   )
 }
---
+++ src/components/Meme.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import memesData from "../memesData";
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom text"
+                />
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
